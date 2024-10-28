644fcee0214e21e6916ee58d2094b4aee5c28da6
Gaston-Care <gecare@udc.edu.ar>
2024-10-28 16:32:23 -0300

    Se creo el directorio 'templates' con la plantillas 'base.html' y la plantilla 'register.html'.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get("username")
+        messages.success(request, f"¡Cuenta creada para {username}!")
+        return redirect("login") # Redireccionar a la página de inicio de sesión o cualquier otra página que desee
+    else:
+        form = UserCreationForm()
+        return render(request, "account/register.html", {"form": form})
